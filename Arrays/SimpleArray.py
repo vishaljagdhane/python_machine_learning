f8497ac96e4c3df6bc725408861b5fcaeb9d00db
Vishal Jagdhane <vishal.jagdhane72@gmail.com>
2025-10-22 12:52:41 +0530

    Done

---
+++ Arrays/SimpleArray.py
@@ -0,0 +1,12 @@
+# What is an Array?
+print("What is an Array?")
+
+# An array is a collection of data stored in a single variable.
+# It allows us to store multiple values in one variable.
+print("An array is a collection of data stored in a single variable. It allows storing multiple values together.")
+
+# Example of a simple array (in Python, it's called a list)
+simple_array = ['Vishal', 'Shubhangi', 'Pooja']
+
+# Printing the array
+print(simple_array)
